3d573c8fc527518fcc3065cdd2f27a20d0b6717c
Carter Pollan <carter.pollan@gmail.com>
2023-10-25 20:33:31 -0600

    added comments

--- lab7_tictactoe/ticTacToeControl.c
+++ lab7_tictactoe/ticTacToeControl.c
@@ -14,6 +14,8 @@
 #define TEXT_SIZE 6
 #define TEXT_LOCATION_X 20
 #define TEXT_LOCATION_Y 100
+#define INIT_MOVE_X 2
+#define INIT_MOVE_Y 2
 
 // States for my State Machine
 typedef enum {
@@ -36,10 +38,14 @@ static tictactoe_board_t board;
 static bool player_is_x;
 static bool first_blood;
 
+// Checking to make sure the spot I want to play is free
 bool check_touch_free(tictactoe_location_t *touch_point) {
+  // Only run if the touchscreen has been touched
   if (touchscreen_get_status()) {
     *touch_point =
         ticTacToeDisplay_getLocationFromPoint(touchscreen_get_location());
+    // If the square that has been touched is empty acknoledge the touch and
+    // return true
     if (board.squares[touch_point->row][touch_point->column] ==
         MINIMAX_EMPTY_SQUARE) {
       touchscreen_ack_touch();
@@ -52,25 +58,30 @@ bool check_touch_free(tictactoe_location_t *touch_point) {
   return false;
 }
 
+// If it is the first move of the game use the hardcoded move, otherwise use
+// minimax to determine the move
 tictactoe_location_t ticTacToeControlComputeNextMove() {
+  // Only use the hardcoded move if it is the first move of the game
   if (first_blood) {
     tictactoe_location_t point;
-    point.column = 2;
-    point.row = 2;
+    point.column = INIT_MOVE_X;
+    point.row = INIT_MOVE_Y;
     return point;
   } else {
     return minimax_computeNextMove(&board, true);
   }
 }
 
+// Clear the screen to prepare for the next game
 void clear_screen() {
   tictactoe_location_t move;
-  // Initalizing board to 0
+  // Itterating over rows
   for (int i = 0; i < TICTACTOE_BOARD_ROWS; i++) {
     move.row = i;
-    // Still initalizing board to 0
+    // Itterating over columns
     for (int j = 0; j < TICTACTOE_BOARD_COLUMNS; j++) {
       move.column = j;
+      // Erase the X or the O depending on which one is currently drawn
       if (board.squares[i][j] == MINIMAX_O_SQUARE) {
         ticTacToeDisplay_drawO(move, true);
       } else if (board.squares[i][j] == MINIMAX_X_SQUARE) {
@@ -102,7 +113,7 @@ void printControlBoard(tictactoe_board_t *Board) {
 // Prints the current state
 void print_states() {
   printf("\n\nCurrent State: ");
-  // More outputs
+  // Prints the name of the state depending on the state
   switch (game_control) {
   case INIT:
     printf("INIT");
@@ -131,11 +142,14 @@ void print_states() {
   }
   printf("\t");
   printf("\n");
+  // Print the move the computer is using
   if (player_is_x) {
+    // Only do it if it is the right time
     if (game_control == CHECK_O) {
       printf("O's move: (%i,%i)", o_move.row, o_move.column);
     }
   } else {
+    // Only do it if it is the right time
     if (game_control == CHECK_X) {
       printf("X's move: (%i,%i)", x_move.row, x_move.column);
     }
@@ -156,6 +170,7 @@ void ticTacToeControl_tick() {
     display_println(WELCOME_MESSAGE);
     break;
   case MENUE:
+    // Display the menue for 3 seconds before moving on
     if (wait_time >= WAIT_TICKS) {
       display_setCursor(TEXT_LOCATION_X, TEXT_LOCATION_Y);
       display_setTextColor(DISPLAY_DARK_BLUE);
@@ -166,6 +181,8 @@ void ticTacToeControl_tick() {
     }
     break;
   case WAIT:
+    // Wait for the player to play, but if they don't for 3 seconds then let the
+    // computer play (whoever plays first is X)
     if (touchscreen_get_status()) {
       game_control = X_TURN;
       player_is_x = true;
@@ -175,10 +192,13 @@ void ticTacToeControl_tick() {
       player_is_x = false;
       wait_time = 0;
     }
-    first_blood = true;
     break;
   case X_TURN:
+    // If the player is X then do the player stuff, otherwise let the computer
+    // decide what to play
     if (player_is_x) {
+      // Wait for the player to make a valid move, draw it to the screen, then
+      // make the move and move on
       if (check_touch_free(&x_move)) {
         ticTacToeDisplay_drawX(x_move, false);
         board.squares[x_move.row][x_move.column] = MINIMAX_X_SQUARE;
@@ -195,12 +215,16 @@ void ticTacToeControl_tick() {
     }
     break;
   case O_TURN:
+    // If the player is O then do the player stuff, otherwise let the computer
+    // decide what to play
     if (player_is_x) {
       o_move = minimax_computeNextMove(&board, true);
       ticTacToeDisplay_drawO(o_move, false);
       board.squares[o_move.row][o_move.column] = MINIMAX_O_SQUARE;
       game_control = CHECK_O;
     } else {
+      // Wait for the player to make a valid move, draw it to the screen, then
+      // make the move and move on
       if (check_touch_free(&o_move)) {
         ticTacToeDisplay_drawO(o_move, false);
         board.squares[o_move.row][o_move.column] = MINIMAX_O_SQUARE;
@@ -210,6 +234,7 @@ void ticTacToeControl_tick() {
     }
     break;
   case CHECK_X:
+    // Check to see if the game is over or not
     if (minimax_computeBoardScore(&board, true) != MINIMAX_NOT_ENDGAME) {
       game_control = END_GAME;
     } else {
@@ -217,6 +242,7 @@ void ticTacToeControl_tick() {
     }
     break;
   case CHECK_O:
+    // Check to see if the game is over or not
     if (minimax_computeBoardScore(&board, false) != MINIMAX_NOT_ENDGAME) {
       game_control = END_GAME;
     } else {
@@ -224,6 +250,7 @@ void ticTacToeControl_tick() {
     }
     break;
   case END_GAME:
+    // If the right button is pressed then restart the game
     if (buttons_read() & 1) {
       game_control = WAIT;
       wait_time = 0;
@@ -241,6 +268,7 @@ void ticTacToeControl_tick() {
     wait_time++;
     break;
   case WAIT:
+    first_blood = true;
     wait_time++;
     break;
   case X_TURN:
@@ -254,7 +282,7 @@ void ticTacToeControl_tick() {
   case END_GAME:
     break;
   }
-
+  // If the state has changed print the debug statements
   if (last_state != game_control) {
     last_state = game_control;
     print_states();
