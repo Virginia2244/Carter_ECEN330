73491e9a13dca3a2d00f1fc6c8c9a5ad80330dd0
Carter Pollan <carter.pollan@gmail.com>
2023-10-29 16:32:35 -0600

    init of lab8 files

---
+++ lab8_missilecommand/plane.c
@@ -0,0 +1,15 @@
+#include "plane.h"
+
+// Initialize the plane state machine
+// Pass in a pointer to the missile struct (the plane will only have one
+// missile)
+void plane_init(missile_t *plane_missile) {}
+
+// State machine tick function
+void plane_tick() {}
+
+// Trigger the plane to expode
+void plane_explode() {}
+
+// Get the XY location of the plane
+display_point_t plane_getXY() {}
